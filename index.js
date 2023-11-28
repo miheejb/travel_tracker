13eeb63dba2f62de92be098929daf957a1d92dec
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-11-29 12:08:44 +1300

    using postgresql

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "world",
+  password: "postit13!",
+  port: 5432,
+});
+db.connect();
+
+let selectedCountries = [];
+
+db.query("SELECT country_code FROM visited_countries", (err,res)=>{
+  if(err){
+    console.error("Error executing query", err.stack);
+  } else {
+    selectedCountries.push;
+  }
+  db.end();
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  res.render("index.ejs", {countries:selectedCountrie, total: selectedCountries.length})
+  
 });
 
 app.listen(port, () => {
